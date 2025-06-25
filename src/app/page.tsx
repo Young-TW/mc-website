6c751aea0a16f39b382e7edd67fcf74305a5629d
Young <young20050727@gmail.com>
2025-06-25 21:04:38 +0800

    update page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ const links = [
   },
   {
     title: "Server",
-    href: "https://mc.young-tw.com",
-    description: "Copy the link mc.young-tw.com to join our server!",
+    href: "https://mc.youn.gg",
+    description: "Copy the link mc.youn.gg to join our server!",
   },
   {
     title: "Contact",
-    href: "https://youngtw.net",
+    href: "https://youn.gg",
     description: "Contact us if you have any question!",
   },
   {
     title: "Donate",
-    href: "https://donate.young-tw.com",
+    href: "https://donate.youn.gg",
     description: "Donate to support our server!",
   },
 ];
@@ -28,7 +28,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-hero bg-fixed bg-no-repeat bg-center bg-cover">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Join mc.young-tw.com now!
+          Join mc.youn.gg now!
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
